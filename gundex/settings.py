cc0fb94f9bbf53aa709c81cb9632a8f784dcc2ce
rasyadzulham <rasyad2712@gmail.com>
2025-10-13 17:56:16 +0700

    feat: add main page, add css styling using tailwind

--- gundex/settings.py
+++ gundex/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'gundex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
