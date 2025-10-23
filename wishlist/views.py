fe7e99d00271a1b8d4619f515573a9123b5509d1
Muhammad Alfa <alphambrk@gmail.com>
2025-10-23 17:34:03 +0700

    refactor(wishlist): update login_required decorator with correct login URL

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from .models import WishlistItem
 from explore_gunung.models import Gunung # Import model Gunung lagi
 import json
 
 # Hanya bisa diakses oleh user yang sudah log in
-@login_required(login_url='/login/')
+@login_required(login_url=reverse_lazy('userprofile:login'))
 def show_wishlist(request):
     # Ambil semua item wishlist HANYA untuk user yang sedang login
     items = WishlistItem.objects.filter(user=request.user)
@@ -18,7 +19,7 @@ def show_wishlist(request):
     return render(request, 'wishlist.html', context)
 
 # Fungsi ini untuk dipanggil oleh AJAX (saat user klik "Add" di hal. Explore)
-@login_required(login_url='/login/')
+@login_required(login_url=reverse_lazy('userprofile:login'))
 def add_to_wishlist_ajax(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -35,7 +36,7 @@ def add_to_wishlist_ajax(request):
     return JsonResponse({'status': 'failed', 'message': 'Request tidak valid.'}, status=400)
 
 # Fungsi untuk menghapus dari wishlist (via AJAX dari halaman wishlist)
-@login_required(login_url='/login/')
+@login_required(login_url=reverse_lazy('userprofile:login'))
 def remove_from_wishlist_ajax(request, item_id):
     if request.method == 'POST':
         item = get_object_or_404(WishlistItem, pk=item_id, user=request.user) # Pastikan item milik user
