2aed7a52f9167a721ce356503d24a2211d786a06
Muhammad Alfa <alphambrk@gmail.com>
2025-10-24 15:01:23 +0700

    feat(wishlist): integrate AJAX-based wishlist toggle in explore page

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'wishlist' # Penting untuk namespacing
+app_name = 'wishlist'
 
 urlpatterns = [
     path('', views.show_wishlist, name='show_wishlist'),
+    path('json/', views.get_wishlist_json, name='get_wishlist_json'),  # Endpoint JSON baru
     path('add/', views.add_to_wishlist_ajax, name='add_to_wishlist_ajax'),
     path('remove/<int:item_id>/', views.remove_from_wishlist_ajax, name='remove_from_wishlist_ajax'),
 ]
