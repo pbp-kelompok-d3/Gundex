f28ad07bc88652bd9bd478f6fa2846629d5d626a
Raithser <m.adella@ui.ac.id>
2025-10-24 11:00:46 +0700

    admin.py diubah

--- artikel/admin.py
+++ artikel/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Artikel
 
-# Register your models here.
+@admin.register(Artikel)
+class ArtikelAdmin(admin.ModelAdmin):
+    list_display  = ("title", "created_at", "updated_at", "views", "total_likes")
+    search_fields = ("title", "description")
+    list_filter   = ("created_at",)
+    ordering      = ("-created_at",)
