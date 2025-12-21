be70ea4b9d4d1806f50f8e5b3d5bb251760855d6
rasyadzulham <rasyad2712@gmail.com>
2025-12-21 17:52:16 +0700

    fix cors problem

--- gundex/settings.py
+++ gundex/settings.py
@@ -189,7 +189,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://10.0.2.2:8000",
+    "https://rasyad-zulham-gundex.pbp.cs.ui.ac.id"
 ]
+
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = False
