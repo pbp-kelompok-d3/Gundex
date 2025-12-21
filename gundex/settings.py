c40454c03f5ad32c6afce70afa2494212eaa7866
rasyadzulham <rasyad2712@gmail.com>
2025-12-21 17:58:58 +0700

    fix corsheader #2

--- gundex/settings.py
+++ gundex/settings.py
@@ -184,18 +184,8 @@ else:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True  
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://10.0.2.2:8000",
     "https://rasyad-zulham-gundex.pbp.cs.ui.ac.id"
 ]
-
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CSRF_COOKIE_SECURE = False
-SESSION_COOKIE_SECURE = False
-
-CSRF_COOKIE_SAMESITE = 'Lax'
-SESSION_COOKIE_SAMESITE = 'Lax'
